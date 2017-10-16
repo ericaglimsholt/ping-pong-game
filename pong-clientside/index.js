9dcf45fb2d8b8ca0383330f09ec55e5c256ae7cc
Marie Eriksson <eriksson.km@gmail.com>
2017-10-16 11:29:14 +0200

    draw ball

--- pong-clientside/index.js
+++ pong-clientside/index.js
@@ -7,6 +7,10 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+function randomInt(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 class Player {
   constructor(color, x, y) {
     this.color = color;
@@ -14,43 +18,52 @@ class Player {
     this.y = y;
     this.height = 200;
     this.width = 30;
-    this.velocity = { up: -5, down: 5 };
+    this.velocity = { up: -10, down: 10 };
   }
 }
+  let ball = {
+    radius: 20,
+    color: '#fff',
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    velocity: {
+      x: 5,
+      y: randomInt(-5, 5)
+    }
+  };
 
-const playerOne = new Player('#35999e', 0, canvas.height / 2 - 100);
-const playerTwo = new Player('#35999e', canvas.width - 30, canvas.height / 2 - 100);
 
-let buttonPressedPlayerOne = null;
-let buttonPressedPlayerTwo = null;
+const playerOne = new Player('#00cc66', 0, canvas.height / 2 - 100);
+const playerTwo = new Player('#fff44f', canvas.width - 30, canvas.height / 2 - 100);
+
+let playerOneButtons = {
+  'AL': false,
+  'AR': false
+}
+
+let playerTwoButtons = {
+  'BL': false,
+  'BR': false
+}
 
 window.addEventListener('keydown', (event) => {
-  if (event.keyCode === 65) {
-    buttonPressedPlayerOne = 'AL'
-  }
-  if (event.keyCode === 68) {
-    buttonPressedPlayerOne = 'AR'
-  }
-  if (event.keyCode === 37) {
-    buttonPressedPlayerTwo = 'BL'
-  }
-  if (event.keyCode === 39) {
-    buttonPressedPlayerTwo = 'BR'
-  }
+  if (event.keyCode === 65) playerOneButtons.AL = true;
+  if (event.keyCode === 68) playerOneButtons.AR = true;
+  if (event.keyCode === 37) playerTwoButtons.BL = true;
+  if (event.keyCode === 39) playerTwoButtons.BR = true;
 });
 
 window.addEventListener('keyup', (event) => {
-  if (event.keyCode === 65 || event.keyCode === 68) {
-    buttonPressedPlayerOne = null;
-  }
-  if (event.keyCode === 37 || event.keyCode === 39) {
-    buttonPressedPlayerTwo = null;
-  }
+  if (event.keyCode === 65) playerOneButtons.AL = false;
+  if (event.keyCode === 68) playerOneButtons.AR = false;
+  if (event.keyCode === 37) playerTwoButtons.BL = false;
+  if (event.keyCode === 39) playerTwoButtons.BR = false;
+
 });
 
 (function mainLoop () {
   // set board background
-  context.fillStyle = '#ccffe9';
+  context.fillStyle = '#333';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   context.beginPath();
@@ -61,18 +74,35 @@ window.addEventListener('keyup', (event) => {
   context.fillStyle = playerTwo.color;
   context.fillRect(playerTwo.x, playerTwo.y, playerTwo.width, playerTwo.height);
 
-  if (buttonPressedPlayerOne === 'AL') {
-    playerOne.y += playerOne.velocity.down;
-  }
-  if (buttonPressedPlayerOne === 'AR') {
-    playerOne.y += playerOne.velocity.up;
-  }
-  if (buttonPressedPlayerTwo === 'BL') {
-    playerTwo.y += playerTwo.velocity.down;
-  }
-  if (buttonPressedPlayerTwo === 'BR') {
-    playerTwo.y += playerTwo.velocity.up;
-  }
+  // draw ball
+  context.beginPath();
+  context.fillStyle = ball.color;
+  context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI, false);
+  context.fill();
 
+  // Update the x and y cordinates based on the velocity.
+  ball.x += ball.velocity.x;
+  ball.y += ball.velocity.y;
+
+  if (playerOneButtons.AL) {
+    if (playerOne.y + playerOne.height < canvas.height) {
+      playerOne.y += playerOne.velocity.down;
+    }
+  }
+  if (playerOneButtons.AR) {
+    if (playerOne.y > 0) {
+      playerOne.y += playerOne.velocity.up;
+    }
+  }
+  if (playerTwoButtons.BL) {
+    if (playerTwo.y + playerTwo.height < canvas.height) {
+      playerTwo.y += playerTwo.velocity.down;
+    }
+  }
+  if (playerTwoButtons.BR) {
+    if (playerTwo.y > 0) {
+      playerTwo.y += playerTwo.velocity.up;
+    }
+  }
   window.requestAnimationFrame(mainLoop);
 })();
