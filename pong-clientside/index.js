6959a67d4f2f8639648d711670179329905ec456
Marie Eriksson <eriksson.km@gmail.com>
2017-10-16 15:32:23 +0200

    fix structure

--- pong-clientside/index.js
+++ pong-clientside/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
+// create canvas game board
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 const context = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// get all the html elements we need
+const startInstructionsBox = document.querySelector('.start')
+const winnerBox = document.querySelector('.winner');
+const restartButton = document.querySelector('.restart');
+const playerOneScoreBoard = document.querySelector('.player-one');
+const playerTwoScoreBoard = document.querySelector('.player-two');
+
+// helper functions
 function randomInt (min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -14,41 +22,42 @@ function randomDirection (left, right) {
   return Math.random() < 0.5 ? right : left;
 }
 
+// Create ball
 let ball = {
   radius: 20,
   color: '#fff',
   x: canvas.width / 2,
   y: canvas.height / 2,
   velocity: {
-    x: randomDirection(-5, 5),
-    y: randomInt(-5, 5)
+    x: randomDirection(-2, 2),
+    y: randomInt(-2, 2)
   }
 };
 
+// Create player rackets
 class Player {
   constructor (color, x, y) {
     this.color = color;
     this.x = x;
     this.y = y;
     this.height = 200;
-    this.width = 30;
+    this.width = 15;
     this.velocity = { up: -10, down: 10 };
   }
 }
-
 const playerOne = new Player('#00cc66', 0, canvas.height / 2 - 100);
-const playerTwo = new Player('#fff44f', canvas.width - 30, canvas.height / 2 - 100);
+const playerTwo = new Player('#fff44f', canvas.width - 15, canvas.height / 2 - 100);
 
 let playerOneButtons = {
   'AL': false,
   'AR': false
 }
-
 let playerTwoButtons = {
   'BL': false,
   'BR': false
 }
 
+// If the players press the valid button, set button to true
 window.addEventListener('keydown', (event) => {
   if (event.keyCode === 65) playerOneButtons.AL = true;
   if (event.keyCode === 68) playerOneButtons.AR = true;
@@ -56,6 +65,7 @@ window.addEventListener('keydown', (event) => {
   if (event.keyCode === 39) playerTwoButtons.BR = true;
 });
 
+// Listen to the players keyup, set button to false
 window.addEventListener('keyup', (event) => {
   if (event.keyCode === 65) playerOneButtons.AL = false;
   if (event.keyCode === 68) playerOneButtons.AR = false;
@@ -63,18 +73,21 @@ window.addEventListener('keyup', (event) => {
   if (event.keyCode === 39) playerTwoButtons.BR = false;
 });
 
+// If press spacebar the game begin
 let gameStarted = false;
 window.addEventListener('keyup', (event) => {
   if (event.keyCode === 32) {
+    startInstructionsBox.classList.remove('show');
     gameStarted = true;
   }
 });
 
+// Score counter
 let scorePlayerOne = 0;
 let scorePlayerTwo = 0;
 
-(function mainLoop () {
-  // set board background
+function mainLoop () {
+  // draw board background
   context.fillStyle = '#333';
   context.fillRect(0, 0, canvas.width, canvas.height);
   // draw player one
@@ -92,7 +105,7 @@ let scorePlayerTwo = 0;
   context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI, false);
   context.fill();
 
-  // players
+  // players movement - check if buttons are pressed
   if (playerOneButtons.AL) {
     if (playerOne.y + playerOne.height < canvas.height) {
       playerOne.y += playerOne.velocity.down;
@@ -126,38 +139,61 @@ let scorePlayerTwo = 0;
       ball.velocity.y = Math.abs(ball.velocity.y);
     }
     // Check if the ball collides with player one
-    if (ball.y > playerOne.y && ball.y < playerOne.y + playerOne.height && ball.x - 30 <= ball.radius) {
+    if (ball.y + ball.radius >= playerOne.y && ball.y + ball.radius <= playerOne.y + playerOne.height && ball.x - 15 <= ball.radius) {
       ball.velocity.x = Math.abs(ball.velocity.x - 1);
     }
     // Check if the ball collides with player two
-    if (ball.y > playerTwo.y && ball.y < playerTwo.y + playerTwo.height && ball.x + ball.radius + 30 > canvas.width) {
+    if (ball.y + ball.radius >= playerTwo.y && ball.y + ball.radius <= playerTwo.y + playerTwo.height && ball.x + ball.radius + 15 >= canvas.width) {
       ball.velocity.x = -ball.velocity.x - 1;
     }
-    // start new round if ball hits right or left
-    if (ball.x + ball.radius < 0 || ball.x - ball.radius > canvas.width) {
-      if (ball.x + ball.radius < 0) {
+    // start new round if one of the platyers misses the ball
+    if (ball.x + ball.radius < -5 || ball.x - ball.radius > canvas.width + 5) {
+      if (ball.x + ball.radius < -5) {
         scorePlayerTwo++;
-        document.querySelector('.player-two').innerText = scorePlayerTwo;
+        playerTwoScoreBoard.innerText = scorePlayerTwo;
       } else {
         scorePlayerOne++;
-        document.querySelector('.player-one').innerText = scorePlayerOne;
+        playerOneScoreBoard.innerText = scorePlayerOne;
       }
       gameStarted = false;
-      const winnerBox = document.querySelector('.winner');
-      if (scorePlayerOne >= 5) {
-        winnerBox.classList.add('show');
-        winnerBox.firstChild.innerText = 'Green is the winner';
-      } else if (scorePlayerTwo >= 5) {
-        winnerBox.classList.add('show');
-        winnerBox.firstChild.innerText = 'Yellow is the winner';
-      } else {
-        ball.x = canvas.width / 2;
-        ball.y = canvas.height / 2;
-        ball.velocity.x = randomDirection(-5, 5);
-        ball.velocity.y = randomInt(-5, 5);
-      }
+      // put ball back in the center to get ready for a new round
+      ball.x = canvas.width / 2;
+      ball.y = canvas.height / 2;
+      ball.velocity.x = randomDirection(-5, 5);
+      ball.velocity.y = randomInt(-5, 5);
     }
   }
 
-  window.requestAnimationFrame(mainLoop);
-})();
+  // Check if one of the players has 5 points, then the game is finished
+  if (scorePlayerOne >= 5) {
+    winnerBox.classList.add('show');
+    winnerBox.firstChild.innerText = 'Green is the winner';
+  } else if (scorePlayerTwo >= 5) {
+    winnerBox.classList.add('show');
+    winnerBox.firstChild.innerText = 'Yellow is the winner';
+  } else {
+    window.requestAnimationFrame(mainLoop);
+  }
+}
+mainLoop();
+
+// reset positions and scores when starting a new game
+restartButton.addEventListener('click', function (event) {
+  scorePlayerOne = 0;
+  scorePlayerTwo = 0;
+  playerOneScoreBoard.innerText = scorePlayerOne;
+  playerTwoScoreBoard.innerText = scorePlayerTwo;
+
+  winnerBox.classList.remove('show');
+  startInstructionsBox.classList.add('show');
+
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+  ball.velocity.x = randomDirection(-5, 5);
+  ball.velocity.y = randomInt(-5, 5);
+
+  playerOne.y = canvas.height / 2 - 100;
+  playerTwo.y = canvas.height / 2 - 100;
+
+  mainLoop();
+});
